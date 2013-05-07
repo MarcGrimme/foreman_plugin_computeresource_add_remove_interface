772403395a70330c3bd319cd69d955ae50c0fd1a
Marc Grimme <grimme@atix.de>
2013-05-07 17:26:56 +0200

    added mac method that returns the first nic mac if not build otherwise the mac of the install nic.

--- app/models/foreman_plugin_computeresource_add_remove_interface/host_extensions.rb
+++ app/models/foreman_plugin_computeresource_add_remove_interface/host_extensions.rb
@@ -77,6 +77,15 @@ module ForemanPluginComputeresourceAddRemoveInterface
          end
        end
 
+       def mac
+         interface=get_interface_2remove
+         if interface
+           interface.mac
+         else
+           super
+         end
+       end
+
        def valueInSetting setting, obj
          if setting.is_a? Hash
            setting.each do | name, value |
